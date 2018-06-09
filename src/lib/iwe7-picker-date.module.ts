002a57ad780c3effb03a94b02ed0ea42e8bec64f
iwe7 <admin@meepo.com.cn>
2018-06-09 16:40:10 +0800

    update

--- src/lib/iwe7-picker-date.module.ts
+++ src/lib/iwe7-picker-date.module.ts
@@ -2,7 +2,7 @@ import { PickerDateComponent } from './picker-date/picker-date';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Iwe7PickerModule } from '../../../iwe7-picker/src/public_api';
+import { Iwe7PickerModule } from 'iwe7-picker';
 
 @NgModule({
   imports: [
